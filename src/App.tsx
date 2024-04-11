4a16dd1bc3bb0c3b92a3c7fe65ca16bca390569c
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-04-11 17:49:54 -0300

    Criado as rotas principais de navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Calculadora from "./pages/Calculadora"
+import Resultado from "./pages/Resultado"
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Calculadora />} />
+        <Route path="/resultado" element={<Resultado />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
